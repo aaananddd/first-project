aff4942a54570486f34bdadc2f9694e75f79465c
ANAND <anandchandran3306@gmail.com>
2024-03-19 11:54:03 +0530

    useState -(onClick-OnDoubleClick)

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Header from "./component/Header";
 import Footer from "./component/Footer";
 import sun from "../public/images/inte.jpg";
 import inter from "../public/images/inta.jpg";
+import MyButton from "./component/MyButton";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faArrowRight } from "@fortawesome/free-solid-svg-icons"; // Replace 'faIcons' with the name of the icon you want to use
 
@@ -36,25 +37,25 @@ const App = () => {
         <div classname="relative">
           <img src={sun} className="w-full" alt="sample image" />
         </div>
-        <div className="absolute inset-0 items-center p-40">
+        <div className="absolute inset-0  flex flex-col justify-center items-center h-2/3 pt-40 text-center">
           <h1 className="text-white font-bold text-7xl p-8">
-            Bringing beauty to every corner
+            Bringing beauty to every corner 
           </h1>
-          <div className='flex flex-col items-center'>
-          <p className='font-semibold text-green-900 text-lg w-1/2 text-center'>Where creativity flourishes and functionality reigns, we design spaces that reflect your unique style and enhance your lifestyle.</p>
-        </div>
+
+          <p className='font-semibold text-green-900 text-lg w-1/2 '>Where creativity flourishes and functionality reigns, we design spaces that reflect your unique style and enhance your lifestyle.</p>
+        
         </div>
         
         <div>
           <h1 className="font-bold text-center text-4xl p-8">
-            One-stop shop for all things interiors
+            One-stop shop for all things interiors 
           </h1>
           <p className="text-center text-l p-1">
             Be it end-to-end interiors, renovation or modular solutions, we have
             it all for your home or office. With a wide range of furniture &
             decor, we have your back from start to finish.
           </p>
-        </div>
+        </div>        
         <div className="flex p-4">
           {tiles.map((item) => (
             <div className="w-1/4 bg-slate-200 rounded-lg p-6 m-4">
@@ -74,6 +75,7 @@ const App = () => {
             </div>
           ))}
         </div>
+        <div className="flex  flex-col items-center font-bold text-3xl w-full justify-center"><MyButton /></div>
       </div>
       <Footer name={"Anand"} />
     </div>
--- src/component/MyButton.jsx
+++ src/component/MyButton.jsx
@@ -1,6 +1,24 @@
-function MyButton() {
-    return(
-      <button>this is a button</button>
-    );
+// import React from 'react'
+import React, { useState } from 'react';
+
+const MyButton = () => {
+  const [colour, setColour] = useState("GREEN");
+  // const [textColour, setTextColour] = useState("text-coll")
+
+  const changeColour = () =>{
+    setColour("RED");
+  };
+  const dbChangeColour =  () => {
+    setColour("GREEN");
   }
-  
+
+  return (
+    <>
+    <h1 className="text-red-500">useState Hook in React</h1>
+    <h2>My favourite color is "{colour}"</h2>
+    <button className="rounded-lg btn-active font-medium p-2 m-3 btn-success" onClick={changeColour} onDoubleClick={dbChangeColour}>Click Here</button>
+    </>
+  );
+};
+
+export default MyButton
