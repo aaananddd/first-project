c0213a15ad74a4b45377191cb35c037a0dc13827
ANAND <anandchandran3306@gmail.com>
2024-03-19 12:11:18 +0530

    changed clr using if condition

--- src/component/MyButton.jsx
+++ src/component/MyButton.jsx
@@ -3,20 +3,22 @@ import React, { useState } from 'react';
 
 const MyButton = () => {
   const [colour, setColour] = useState("GREEN");
-  // const [textColour, setTextColour] = useState("text-coll")
+  const [textColour, setTextColour] = useState("text-green-500")
 
   const changeColour = () =>{
-    setColour("RED");
+    if(colour=="RED"){
+      setColour("GREEN")
+    }else{
+      setColour("RED")
+    }
   };
-  const dbChangeColour =  () => {
-    setColour("GREEN");
-  }
+  
 
   return (
     <>
     <h1 className="text-red-500">useState Hook in React</h1>
-    <h2>My favourite color is "{colour}"</h2>
-    <button className="rounded-lg btn-active font-medium p-2 m-3 btn-success" onClick={changeColour} onDoubleClick={dbChangeColour}>Click Here</button>
+    <h2>My favourite color is <h2 className={colour==="RED"?"text-red-500":"text-green-500"}>"{colour}"</h2></h2>
+    <button className="rounded-lg btn-active font-medium p-2 m-3 btn-success" onClick={changeColour}>Click Here</button>
     </>
   );
 };
